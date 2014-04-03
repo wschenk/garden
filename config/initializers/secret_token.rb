7e4ad238bc3e3bc2cc77239f1a39cdbb3c699acc
Will Schenk <wschenk@gmail.com>
2014-04-03 17:29:37 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Garden::Application.config.secret_key_base = 'e34f1ef26316effa42c79b274b5668d469de1bda76544531e029f9981dd27d35edd347ffff23c44260fde5435f79c7563e444de207906ac0fb9e27ecfadb557e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Garden::Application.config.session_store :cookie_store, key: '_garden_session'
